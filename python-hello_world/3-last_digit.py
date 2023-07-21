e86c6db310fbc5ddf27fe80a8a6842576da38d10
Luzanne <86235334+Luzanne-S@users.noreply.github.com>
2023-07-21 19:52:19 +0200

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,13 +1,12 @@
-
+#!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
 last_num = abs(number) % 10
-last_num = -last_num
 if last_num > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last_num))
 elif last_num == 0:
     print("Last digit of {} is {} and is 0".format(number, last_num))
-else:
+elif last_num < 6 and last_num != 0:
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_num))
 
-   
+#how to add - to negative numbers    
